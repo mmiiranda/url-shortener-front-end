c5cc0a17f6fbd6db1d725820d5dc414e6c14326a
mmiiranda <mauricio201190b@gmail.com>
2024-10-12 15:44:27 -0300

    feat: adding Catch View Logic

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import CatchView from '../views/CatchView.vue'
 
 const routes = [
   {
@@ -7,14 +8,11 @@ const routes = [
     name: 'home',
     component: HomeView
   },
-  // {
-  //   path: '/about',
-  //   name: 'about',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  // }
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'CatchURL',
+    component: CatchView
+  }
 ]
 
 const router = createRouter({
---
+++ src/views/CatchView.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "CatchView",
+        async beforeRouteEnter() {
+            const url = new URL(window.location.href);
+            
+            try{
+                const response = await fetch(`http://localhost:8080/api${url.pathname}`)
+
+                const data = await response.json();
+                window.location.replace(`https://${data.url}`)    
+            }catch(error){
+                console.error('Ocorreu um erro')
+            }
+        }
+    }
+</script>
