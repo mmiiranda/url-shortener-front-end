e605ef77a1b285ce96bfb727bbd6bdccdd4c116c
mmiiranda <mauricio201190b@gmail.com>
2024-10-12 15:59:22 -0300

    fix: api router

--- src/views/CatchView.vue
+++ src/views/CatchView.vue
@@ -11,7 +11,7 @@
             const url = new URL(window.location.href);
             
             try{
-                const response = await fetch(`http://localhost:8080/api${url.pathname}`)
+                const response = await fetch(`http://localhost:8080/api/url${url.pathname}`)
 
                 const data = await response.json();
                 window.location.replace(`https://${data.url}`)    
